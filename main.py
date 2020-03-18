a5743bae4a3fbb1b219f4a92d7ee9b7f806298d4
Alvi Aziz <aaziz00@stuy.edu>
2020-03-18 02:19:27 -0400

    skelly

---
+++ main.py
@@ -0,0 +1,12 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
+
+parse_file( 'script', edges, transform, screen, color )
